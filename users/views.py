6f7808dbba86206f9cbb0fba553f8f3fd331afdc
Paulina Jaworska <paulinajaworska9@gmail.com>
2020-02-18 21:49:02 +0100

    Added a profile model to store users pictures

--- users/views.py
+++ users/views.py
@@ -8,12 +8,13 @@ def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
-            #username = form.cleaned_data.get('username')
+            # username = form.cleaned_data.get('username')
             messages.success(request, f"Account created has been created. You are now able to log in.")
             return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
 
 @login_required
 def profile(request):
