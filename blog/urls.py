3e55657f62c4ec391f827fbbbba72f553cf6f26a
Paulina Jaworska <paulinajaworska9@gmail.com>
2020-02-20 13:58:05 +0100

    Changed home page view to class based view

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
+from .views import PostListView
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+
 from .models import Post
 
 
-def home(request):
-    context = {
-        'posts': Post.objects.all(),
-        'title': 'Home'
-    }
-    return render(request, 'blog/home.html', context)
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
