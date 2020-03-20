dad35e46605497a2f1379dd1b4950b6c30f8976d
Paulina Jaworska <paulinajaworska9@gmail.com>
2020-03-20 16:55:24 +0100

    Addded webpack settings for css and added navbar

--- frontend/src/components/layout/NavBar.js
+++ frontend/src/components/layout/NavBar.js
@@ -1,4 +1,6 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+
+import '../../../static/css/Navbar.css';
 
 export default class Header extends Component {
     render() {
@@ -22,7 +24,7 @@ export default class Header extends Component {
 
                     <form className="form-inline my-2 my-lg-0">
                         <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-                        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                        <button className="btn btn-light my-2 my-sm-0" type="submit">Search</button>
                     </form>
 
                         </ul>
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,13 +1,18 @@
 module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      }
-    ]
-  }
-};
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.css$/,
+                loaders: ['style-loader', 'css-loader'],
+            }
+        ]
+    }
+}
+;
