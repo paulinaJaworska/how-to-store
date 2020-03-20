bbc4f55b6412eb213f5c8c28116993baf23e8b5a
Paulina Jaworska <paulinajaworska9@gmail.com>
2020-03-20 17:43:54 +0100

    Made navbar responsive

--- frontend/src/components/layout/NavBar.js
+++ frontend/src/components/layout/NavBar.js
@@ -6,6 +6,11 @@ export default class Header extends Component {
     render() {
         return (
             <nav className="navbar navbar-expand-lg navbar-light">
+                <button className="navbar-toggler" type="button" data-toggle="collapse"
+                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <div className="container-fluid">
                     <div className="row w-100">
