9f0020680241b6eeb9623dc76341a2852c700ca6
Paulina Jaworska <paulinajaworska9@gmail.com>
2020-03-20 16:01:05 +0100

    Added bootstrap

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,9 +1,13 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
+import NavBar from './layout/NavBar'
+
 class App extends Component {
     render() {
-        return <h1>React App</h1>
+        return (
+            <NavBar/>
+        )
     }
 }
 
---
+++ frontend/src/components/layout/NavBar.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react'
+
+export default class Header extends Component {
+    render() {
+        return (
+            <nav className="navbar navbar-expand-lg navbar-light">
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <div className="container-fluid">
+                    <div className="row w-100">
+                    <ul className="navbar-nav mr-auto col-2">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">Home<span className="sr-only">(current)</span></a>
+                        </li>
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">About<span className="sr-only">(current)</span></a>
+                        </li>
+                    </ul>
+                    <ul className="navbar-nav mr-auto col-8 d-flex justify-content-center">
+                        <li className="nav-item active">
+                            <h3 className="nav-link">How to store<span className="sr-only">(current)</span></h3>
+                        </li>
+
+                    <form className="form-inline my-2 my-lg-0">
+                        <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+                        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                    </form>
+
+                        </ul>
+                    <ul className="navbar-nav mr-auto col-2 d-flex justify-content-end">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">Login<span className="sr-only">(current)</span></a>
+                        </li>
+                    </ul>
+                    </div>
+                    </div>
+                </div>
+            </nav>
+        )
+    }
+}
