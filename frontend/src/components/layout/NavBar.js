9ae28c296184f4ec8a5ca30bc10745077501e767
Paulina Jaworska <paulinajaworska9@gmail.com>
2020-03-30 19:57:52 +0200

    Small changes to layout structure, added link to Home link

--- frontend/src/components/layout/NavBar.js
+++ frontend/src/components/layout/NavBar.js
@@ -7,6 +7,7 @@ export default class Header extends Component {
     render() {
         return (
             <nav className="navbar navbar-expand-lg navbar-light">
+                <div className="container">
                 <button className="navbar-toggler" type="button" data-toggle="collapse"
                         data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                         aria-expanded="false" aria-label="Toggle navigation">
@@ -17,7 +18,7 @@ export default class Header extends Component {
                         <div className="row w-100">
                             <ul className="navbar-nav mr-auto col-2">
                                 <li className="nav-item active my-2">
-                                    <a className="nav-link" href="#">Home<span className="sr-only">(current)</span></a>
+                                    <a className="nav-link" href="/">Home<span className="sr-only">(current)</span></a>
                                 </li>
                                 <li className="nav-item active my-2">
                                     <a className="nav-link" href="#">About<span className="sr-only">(current)</span></a>
@@ -36,6 +37,7 @@ export default class Header extends Component {
                             </ul>
                         </div>
                     </div>
+                </div>
                 </div>
             </nav>
         )
