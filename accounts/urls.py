53bcf24a6417a7a1f01852626a03d6592e5a3406
Paulina Jaworska <paulinajaworska9@gmail.com>
2020-05-17 00:34:57 +0200

    Added logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('api/auth', include('knox.urls')),
     path('api/auth/register', RegisterAPI.as_view()),
     path('api/auth/login', LoginAPI.as_view()),
-    path('api/auth/user', UserAPI.as_view())
+    path('api/auth/user', UserAPI.as_view()),
+    # Logout invalidates token. It should be done on backend and frontend
+    path('api/auth/logout', knox_views.LogoutView.as_view(), name='knox_logout')
 ]
