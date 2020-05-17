c90f0c12b1bfa61781f130e4e16dccabbf248f0f
Paulina Jaworska <paulinajaworska9@gmail.com>
2020-05-17 14:01:03 +0200

    Refactored App.js router to use Switch

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,54 +1,50 @@
 import React, {Component} from 'react';
 import {Provider} from 'react-redux';
 import store from "../store";
-import {BrowserRouter, Route} from "react-router-dom";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import ReactDOM from "react-dom";
 
 import '../../static/css/main.css';
 import NavBar from './layout/NavBar';
 import FoodImagesList from './food_items/FoodImagesList';
 import FoodDetails from './food_items/FoodDetails';
+import {Alerts} from "./layout/Alerts";
+
+import {Provider as AlertProvider} from 'react-alert';
+import AlertTemplate from 'react-alert-template-basic';
 
 
 class AppParent extends Component {
-    getContent(currentPage) {
-        switch (currentPage) {
-            case 'foodList':
-                return <FoodImagesList/>;
-            case 'foodDetails':
-                return <FoodDetails/>;
-            default:
-                return null;
-        }
-    }
 
     render() {
-        const currentPage = this.props.whichPage;
+        // const currentPage = this.props.whichPage;
+        // return (
+        //     <>
+        //         <div>{this.getContent(currentPage)}</div>
+        //     </>
+        // );
         return (
-            <>
-                <div>{this.getContent(currentPage)}</div>
-            </>
-        );
+            <Provider store={store}>
+                <div className="main">
+                    {/*<AlertProvider template={AlertTemplate} {...alertOptions}>*/}
+                    <BrowserRouter>
+                        <>
+                            <NavBar/>
+                            {/*<Alerts/>*/}
+                            <div className="page-content-container">
+                                <Switch>
+                                    <Route exact path="/" component={FoodImagesList}/>
+                                    <Route exact path="/:id" component={FoodDetails}/>
+                                </Switch>
+                            </div>
+                        </>
+                    </BrowserRouter>
+                    {/*</AlertProvider>*/}
+                </div>
+            </Provider>
+        )
     }
 }
 
-const routing = (
-    <Provider store={store}>
-        <div className="main">
-            <NavBar/>
-            <Alerts />
-            <div className="page-content-container">
-                <BrowserRouter>
-                    <>
-                        <Route path="/" exact render={(props) => <AppParent whichPage='foodList' {...props}/>}/>
-                        <Route path="/:id" exact render={(props) => <FoodDetails {...props}/>}/>
-                        {/*<Route path="/:id" exact render={(props) => <AppParent whichPage='foodDetails' {...props}/>}/>*/}
-                    </>
-                </BrowserRouter>
-            </div>
-        </div>
-    </Provider>
-);
-
-ReactDOM.render(routing, document.getElementById("app"));
+ReactDOM.render(<AppParent/>, document.getElementById("app"));
 
