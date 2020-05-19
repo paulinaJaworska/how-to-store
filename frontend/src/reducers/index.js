ec5b3d52ca93ee6bd0eff671feff7be673ba4709
Paulina Jaworska <paulinajaworska9@gmail.com>
2020-05-19 14:45:48 +0200

    Added auth reducer

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -2,9 +2,11 @@ import {combineReducers} from "redux";
 import foodItems from './foodItems';
 import errors from "./errors";
 import messages from "./messages";
+import auth from "./auth";
 
 export default combineReducers({
     foodItems,
     errors,
     messages,
+    auth
 })
