4dca2d353c269aea4c948911fa62be1a66a1910d
Paulina Jaworska <paulinajaworska9@gmail.com>
2020-05-20 21:10:22 +0200

    Wired login action to Login component

--- frontend/src/components/accounts/Login.js
+++ frontend/src/components/accounts/Login.js
@@ -1,5 +1,8 @@
 import React, {Component} from 'react';
-import {Link} from "react-router-dom";
+import {Link, Redirect} from "react-router-dom";
+import PropTypes from 'prop-types';
+import {connect} from "react-redux";
+import {login} from "../../actions/auth";
 
 class Login extends Component {
     state = {
@@ -7,15 +10,23 @@ class Login extends Component {
         password: '',
     };
 
+    static propTypes = {
+        login: PropTypes.func.isRequired,
+        isAuthenticated: PropTypes.bool
+    };
+
     onSubmit = e => {
         e.preventDefault();
-        console.log("submit")
+        this.props.login(this.state.username, this.state.password);
     };
 
     // in render use the same label to set value eg. name=”username” value={username}
     onChange = e => this.setState({[e.target.name]: e.target.value});
 
     render() {
+        if (this.props.isAuthenticated) {
+            return <Redirect to="/"/>;
+        }
         const {username, password} = this.state;
         return (
             <div className="col-md-6 m-auto">
@@ -57,4 +68,8 @@ class Login extends Component {
     }
 }
 
-export default Login;
+const mapStateToProps = state => ({
+    isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps, {login})(Login);
